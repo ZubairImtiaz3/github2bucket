d37aecec5e66c87e21ea61584f1669b956a1ee64
Zubair Imtiaz <106450046+ZubairImtiaz3@users.noreply.github.com>
2024-04-16 16:55:51 +0500

    mounted repo table

--- app/repositories/page.tsx
+++ app/repositories/page.tsx
@@ -1,15 +1,37 @@
+import { RepoTable } from "@/components/RepoTable";
 import { createClient } from "@/utils/supabase/server";
+import axios from "axios";
 
 const page = async () => {
   const supabase = createClient();
 
+  await supabase.auth.getUser();
   const { data } = await supabase.auth.getSession();
 
-  console.log("sesssion in repo", data);
+  // Extracting the provider_token from the session
+  const providerToken = data.session?.provider_token;
+
+  // try {
+  //   // Making request to GitHub API
+  //   const response = await axios.get("https://api.github.com/user/repos", {
+  //     headers: {
+  //       Authorization: `bearer ${providerToken}`,
+  //     },
+  //   });
+
+  //   console.log("GitHub repositories:", response.data);
+  // } catch (error) {
+  //   console.log(error);
+  // }
 
   return (
     <>
-      <div>Repositories</div>
+      <div className="p-10">
+        <h1 className="text-center">Repositories</h1>
+        <div className="max-w-[90%] mx-auto">
+          <RepoTable />
+        </div>
+      </div>
     </>
   );
 };
